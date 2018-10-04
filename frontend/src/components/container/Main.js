c605c4470a1b54fad51a0dabaf2c2fffe773e28e
totutotu <tuomo.torppa@gmail.com>
2018-10-04 09:52:00 +0300

    Added material UI

--- frontend/src/components/container/Main.js
+++ frontend/src/components/container/Main.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { withRouter, Switch, Route } from 'react-router-dom'
 import Login from './Login'
+import NavBar from './NavBar'
+import LogOut from './LogOut'
 
 const Main = () => (
   <main>
+    <NavBar />
+    <LogOut />
     <Switch>
       <Route exact path="/login" component={Login} />
       <Route component={null} />
