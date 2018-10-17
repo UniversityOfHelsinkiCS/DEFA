15df1e7c6add80755a00f71f95c5ea6a7818f4a4
totutotu <tuomo.torppa@gmail.com>
2018-10-17 12:17:11 +0300

    removed "Hello world" reducer

--- frontend/src/components/container/App.js
+++ frontend/src/components/container/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
 
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
-import { getHello } from '../../util/redux/reducer'
 import ApolloContainer from './ApolloContainer'
 
 const palette = {
@@ -22,24 +19,11 @@ const themeName = 'Turquoise Blue Texas Rose Bandicoot'
 
 const theme = createMuiTheme({ palette, themeName, typography: { useNextVariants: true } })
 
-const App = ({ getHelloFn }) => {
-  getHelloFn()
+const App = () => (
+  <MuiThemeProvider theme={theme}>
+    <ApolloContainer />
+  </MuiThemeProvider>
+)
 
-  return (
-    <MuiThemeProvider theme={theme}>
-      <ApolloContainer />
-    </MuiThemeProvider>
-  )
-}
 
-const { func } = PropTypes
-
-App.propTypes = {
-  getHelloFn: func.isRequired
-}
-
-const mapDispatchToProps = dispatch => ({
-  getHelloFn: () => dispatch(getHello())
-})
-
-export default connect(null, mapDispatchToProps)(App)
+export default App
--- frontend/src/util/redux/index.js
+++ frontend/src/util/redux/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import reducer from './reducer'
 import user from './user'
 import uploadCredits from './uploadCredits'
 
 export default combineReducers({
-  reducer,
   user,
   uploadCredits
 })
