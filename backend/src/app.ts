eeb7ab827a7108ddd1fff95e9738c024f54884a9
Salo Jemi S <jemi.salo@gmail.com>
2018-11-09 18:26:52 +0200

    Give graphiql queries admin privileges in development for easier testing.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,18 @@ import routes from './routes'
 import cors from 'cors'
 import tokenAuth from './middleware/token_authorization'
 import applyBodyParser from './middleware/body_parser'
+import graphiqlUser from './middleware/graphiql_user'
 
 const app: express.Express = express()
 
 app.use(cors())
 
 app.use(tokenAuth)
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('Running in development mode.')
+  app.use(graphiqlUser) // Gives admin privileges to graphiql queries. Absolutely do not use in production.
+}
 
 applyBodyParser(app)
 
