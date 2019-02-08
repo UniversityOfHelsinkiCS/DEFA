e2814afa924e7e05852e16e6ce83bb16e8fac3d5
Salo Jemi S <jemi.salo@gmail.com>
2019-02-08 10:45:11 +0200

    Fix broken frontend test

--- frontend/tests/util/actions/user.test.js
+++ frontend/tests/util/actions/user.test.js
@@ -46,6 +46,6 @@ describe('logout action', () => {
   it('removes DEFA-token item from localStorage.', () => {
     expect(localStorage.getItem('DEFA-token')).toEqual(testToken)
     actions.logout({})(dispatch)
-    expect(localStorage.getItem('DEFA-token')).toEqual(null)
+    expect(localStorage.getItem('DEFA-token')).toBeUndefined()
   })
 })
